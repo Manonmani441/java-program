a518519e9c65ad20b52b51e531a1f31bab346380
Manonmani441 <36624028+Manonmani441@users.noreply.github.com>
2018-06-19 21:45:51 +0530

    Update odd no in interval.java

--- odd no in interval.java
+++ odd no in interval.java
@@ -3,8 +3,8 @@ class A{
 public Static void main(String args[]){
 int n=Integer.parseInt(args[0]);
 int m=Integer.parseint(args[1]);
-for(int i=n+1;i<m;i++){
-if(i%2==1){
+for(int i=n;i<m;i++){
+if((i+1 )%2==1){
 System.out.println(i);
 }
 }
