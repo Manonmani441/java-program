013a54331a6183633cb315874ede062f2689a7a0
Manonmani441 <36624028+Manonmani441@users.noreply.github.com>
2018-06-19 21:32:58 +0530

    Create odd no in interval.java

---
+++ odd no in interval.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class A{
+public Static void main(String args[]){
+int n=Integer.parseInt(args[0]);
+int m=Integer.parseint(args[1]);
+for(int i=n;i<m;i++){
+if(i%2==1){
+System.out.println(i);
+]
+}
+}
+}
