1f692af2d62fabc344a538a8ba6f5dc19e6850e8
Manonmani441 <36624028+Manonmani441@users.noreply.github.com>
2018-06-19 21:07:32 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class A{
+public static void main(String arg[]){
+boolean f=false;
+int n=Integer.parseInt(arg[0]);
+for(i=1;i<n%2;i++){
+if(n%1==0)
+{
+f=true;
+break;
+}
+}
+if(!f)
+System.out.ptintln("prime");
+else
+System.out.println("not prime");
+}
+}
