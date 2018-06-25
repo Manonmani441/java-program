fa400f691b110707e92c2a89fd02eec6f8a9a739
Manonmani441 <36624028+Manonmani441@users.noreply.github.com>
2018-06-25 18:36:09 +0530

    Create prime no.java

---
+++ prime no.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class A{
+public static void main(String arg[]){
+boolean f=false;
+int n=Integer.parseInt(arg[0]);
+for(i=2;i<=n%2;i++){
+if(n%1==0)
+{
+f=true;
+break;
+}
+}
+if(!f)
+System.out.ptintln("yes");
+else
+System.out.println("no");
+}
+}
